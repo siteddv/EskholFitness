8e87b4673f59807bc00c541de5b56cca0b2253f8
Дмитрий Виноградов <eskhol2@live.com>
2020-05-20 23:05:27 +0600

    new

---
+++ EskholFitness.BL/Model/Gender.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EskholFitness.BL.Model
+{
+    public class Gender
+    {
+        public string genderName { get; }
+        public Gender (string genderName)
+        {
+            if (string.IsNullOrWhiteSpace(genderName))
+            {
+                throw new ArgumentNullException("Название пола не можеть быть пустым или null", nameof(genderName));
+            }
+            this.genderName = genderName;
+        }
+        public override string ToString()
+        {
+            return genderName;
+        }
+
+    }
+}
---
+++ EskholFitness.BL/Model/User.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EskholFitness.BL.Model
+{
+    /// <summary>
+    /// Пользователь
+    /// </summary>
+    public class User
+    {
+        #region свойства
+        /// <summary>
+        /// Имя.
+        /// </summary>
+        public string userName { get; private set; }
+        /// <summary>
+        /// Пол.
+        /// </summary>
+        public Gender gender { get; private set; }
+        /// <summary>
+        /// Дата рождения.
+        /// </summary>
+        public DateTime birthDate { get; private set; }
+        /// <summary>
+        /// Вес.
+        /// </summary>
+        public double weight { get; private set; }
+        /// <summary>
+        /// Рост.
+        /// </summary>
+        public double height { get; private set; }
+        /// <summary>
+        /// Создание нового пользователя
+        /// </summary>
+        /// <param name="userName">Имя</param>
+        /// <param name="gender">Пол</param>
+        /// <param name="bithDate">Дата рождения</param>
+        /// <param name="weight">Вес</param>
+        /// <param name="height">Рост</param>
+        #endregion
+        public User(string userName, Gender gender, DateTime bithDate, double weight, double height)
+        {
+            #region Проверка условий
+            if (string.IsNullOrWhiteSpace(userName))
+            {
+                throw new ArgumentNullException("Имя пользователя не может быть пустым или равным null.", nameof(userName));
+            }
+            if(gender == null)
+            {
+                throw new ArgumentNullException("Пол пользователя не может быть null.", nameof(gender));
+            }
+            if(birthDate < DateTime.Parse("0.0.1900") || birthDate > DateTime.Now)
+            {
+                throw new ArgumentException($"Пользователь не можеть быть старше {DateTime.Now.Year-1900} лет.", nameof(birthDate));
+            }
+            if (weight <= 0)
+            {
+                throw new ArgumentException("Вес пользователя не можеть быть меньше или равным нулю.", nameof(weight));
+            }
+            if (height <= 0)
+            {
+                throw new ArgumentException("Рост пользователя не можеть быть меньше или равным нулю.", nameof(height));
+            }
+            #endregion
+            this.userName = userName;
+            this.gender = gender;
+            this.birthDate = birthDate;
+            this.weight = weight;
+            this.height = height;
+        }
+        public override string ToString()
+        {
+            return $"Имя: {userName} Пол: {gender} Дата рождения: {birthDate} Вес: {weight} Рост: {height}";
+        }
+    }
+}
