760c82e9e51d144d9b156dfb46ff0c7a642671d9
Дмитрий Виноградов <eskhol2@live.com>
2020-05-25 08:26:10 +0600

    Добавлен установщик

--- EskholFitness.BL/Controller/BaseController.cs
+++ EskholFitness.BL/Controller/BaseController.cs
@@ -4,7 +4,7 @@ namespace EskholFitness.BL.Controller
 {
     public abstract class BaseController
     {
-        private readonly IDataSaver manager = new SerializeDataSaver();
+        private readonly IDataSaver manager = new SerializeDataSaver();//поменяв SerializeDataSaver на DatabaseDataSaver можно сохранять данные в базу данных
         protected List<T> GetData<T>() where T:class
         {
             return manager.GetData<T>();
